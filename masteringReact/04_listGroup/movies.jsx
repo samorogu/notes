b128992af75b35293a85cc2d3ddec3c249f38ddd
™samorogu™ <samorogu@gmail.com>
2019-01-04 23:37:40 -0600

    adding the missings files of the list group chapter

---
+++ masteringReact/04_listGroup/movies.jsx
@@ -0,0 +1,133 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
+import Like from "../common/like";
+import Pagination from "../common/paginantion";
+import { paginate } from "../utils/paginate";
+import ListGroup from "../common/listGroup";
+class Movies extends Component {
+  state = {
+    movies: [], //until I use lifehooks, this will be the way I will set the state
+    genres: [], //For the purpous of this exercise will get the genres in component did mount
+    currentPage: 1, //current page in the pagination
+    pageSize: 3 //number of pages displayed
+  };
+
+  componentDidMount() {
+    this.setState({ movies: getMovies(), genres: getGenres() });
+  }
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    //in modern javascript when we have this repetition, we can leave only the movie
+    //this.setState({ movies: movies });
+    this.setState({ movies });
+  };
+  //this approach is correct but not quite if we dont want to render the table also
+  /*
+  moviesCount() {
+    const { length: count } = this.state.movies; //object destructuring
+
+    let moviesText = "The total number of movies is ";
+    count !== 0 ? (moviesText += count) : (moviesText = "There are no movies ");
+    return moviesText;
+  }
+  */
+
+  handleLike = movie => {
+    //console.log("movie liked", movie);
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  handleGenreSelect = genre => {
+    //console.log(genre);
+    this.setState({ selectedGenre: genre });
+  };
+
+  render() {
+    const { length: count } = this.state.movies; //object destructuring
+    const {
+      pageSize,
+      currentPage,
+      movies: allMovies,
+      selectedGenre,
+      genres
+    } = this.state;
+    if (count === 0) return <p>There are no movies</p>;
+
+    //before pagination we need to do filtering
+
+    const filtered = selectedGenre
+      ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+      : allMovies;
+    const movies = paginate(filtered, currentPage, pageSize); //if count is not 0 we will create an array of movies
+
+    return (
+      <div className="row">
+        <div className="col-3">
+          <ListGroup
+            items={genres}
+            selectedItem={selectedGenre}
+            onItemSelect={this.handleGenreSelect}
+          />
+        </div>
+
+        <div className="col">
+          <p>There are {filtered.length} movies in the database</p>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th />
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {movies.map(movie => (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      liked={movie.liked}
+                      onClick={() => this.handleLike(movie)}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => this.handleDelete(movie)}
+                      className="btn btn-danger btn-sm ml-2"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Pagination
+            itemsCount={filtered.length}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={this.handlePageChange}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Movies;
---
+++ masteringReact/04_listGroup/paginantion.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import _ from "lodash"; //optimization from underscore
+
+const Pagination = props => {
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+  console.log(currentPage);
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+
+  if (pagesCount === 1) return null;
+
+  const pages = _.range(1, pagesCount + 1); //if pages count is 3, the range will be 2, so we need to increment by 1
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map(page => (
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a
+              className="page-link"
+              /*type="button"
+              className="link-button"
+              style={{
+                backgroundColor: "transparent",
+                border: "none",
+                cursor: "pointer",
+                textDecoration: "underline",
+                display: "inline",
+                margin: "0",
+                padding: "0"
+              }}*/
+              onClick={() => onPageChange(page)}
+            >
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
+};
+
+export default Pagination;
